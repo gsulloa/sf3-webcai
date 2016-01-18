5e3ea603718460ab970e1bcc9f383df5a0c89e28
Gabriel Ulloa <gsulloa@uc.cl>
2016-01-18 13:20:58 -0300

    pagina remodelacion con base de datos

--- src/Cai/FrontendBundle/Controller/DefaultController.php
+++ src/Cai/FrontendBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CaiFrontendBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $contacto = $em->getRepository('CaiWebBundle:Contacto')->find(1);
+        return $this->render('CaiFrontendBundle:Default:index.html.twig',array(
+            'contacto'  => $contacto
+        ));
     }
 }
