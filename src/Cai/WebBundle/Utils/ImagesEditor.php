2682ee61678c9f28e89df4d55d60de8aad041aa9
Gabriel Ulloa <gsulloa@uc.cl>
2016-03-16 03:03:36 -0300

    Solo resize si tamaño final es menor

--- src/Cai/WebBundle/Utils/ImagesEditor.php
+++ src/Cai/WebBundle/Utils/ImagesEditor.php
@@ -20,7 +20,10 @@ class ImagesEditor
     function resize($image, $final_images){
         $thumb = new \Imagick($image);
         foreach($final_images as $final_image){
-            $thumb->scaleImage($final_image['width'], $final_image['height'],true);
+            $size = $thumb->getImageGeometry();
+            if($size['width'] > $final_image['width'] || $size['height'] > $final_image['height'] ) {
+                $thumb->scaleImage($final_image['width'], $final_image['height'], true);
+            }
             $thumb->writeImage($final_image['final_name']);
         }
         $thumb->destroy();
